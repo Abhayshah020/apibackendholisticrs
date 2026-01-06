e35902257f45dc221966c6550b68cea476fc88b4
Abhay Shah <seraski2021@gmail.com>
2026-01-07 00:34:53 +0545

    complete

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(cors({
     origin: [
-        "http://localhost:3000",
+        "https://holisticrs.com.au/",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],

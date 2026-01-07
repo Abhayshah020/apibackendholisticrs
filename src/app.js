8ef00a2a9de5638dff83a2dae71f0babc95ea739
Abhay Shah <seraski2021@gmail.com>
2026-01-07 23:47:59 +0545

    copleted

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5024;
         await sequelize.authenticate();
         console.log("✅ PostgreSQL connected");
 
-        await sequelize.sync();
+        // await sequelize.sync();
         console.log("✅ Models synced");
 
         server.listen(PORT, () => {
